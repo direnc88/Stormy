eda6df9e2e3fc7a5a54fe9f3bbd3aa82eacb5bd5
direnc88 <charlesdirenzo@gmail.com>
2017-08-25 14:39:30 -0500

    Added some buttons so that teh user can change to an hourly and 7 day outlook of the forecast.

---
+++ app/src/main/java/com/treehouse/android/stormy/adapters/DayAdapter.java
@@ -0,0 +1,73 @@
+package com.treehouse.android.stormy.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.treehouse.android.stormy.weather.Day;
+
+
+import static com.google.android.gms.internal.a.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by benjakuben on 2/6/15.
+ */
+public class DayAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days) {
+        mContext = context;
+        mDays = days;
+    }
+
+    @Override
+    public int getCount() {
+        return mDays.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDays[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; // we aren't going to use this. Tag items for easy reference
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            // brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView; // public by default
+        TextView temperatureLabel;
+        TextView dayLabel;
+    }
+}
